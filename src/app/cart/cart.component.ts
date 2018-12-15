4af45e9ba7fd04e196e8351627c2545b0138183e
Thomas <thom170b@easv365.dk>
2018-12-15 07:03:11 +0100

    Made cart work correctly - It is now showing only 1 of every product, but shows the amount of it in the right place - Delete is dynamic and responsive and the row deletes when it hits 0 of the specific product in the cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {CartService} from '../shared/services/cart.service';
 import {Product} from '../shared/models/product';
+import {CartProduct} from '../shared/models/cartProduct';
 
 @Component({
   selector: 'app-cart',
@@ -10,6 +11,7 @@ import {Product} from '../shared/models/product';
 export class CartComponent implements OnInit {
 
   cart: Product[] = [];
+  cartProducts: CartProduct[] = [];
 
   constructor(private cartService: CartService) {
   }
@@ -20,6 +22,22 @@ export class CartComponent implements OnInit {
     const allProductIds = this.cart.map(p => p.id).filter((value, index, array) => index === array.indexOf(value));
     for (const allProductId of allProductIds) {
       const amountOfSpecificProduct = this.cart.filter(value => value.id === allProductId).length;
+
+      const cartProduct: CartProduct = {
+        product: this.cart.find(value => value.id === allProductId),
+        numberOfProduct: amountOfSpecificProduct
+      };
+
+      this.cartProducts.push(cartProduct);
+    }
+  }
+
+  removeFromCart(product: Product) {
+    this.cartService.removeFromCart(product);
+    this.cartProducts.find(value => value.product === product).numberOfProduct--;
+
+    if (this.cartProducts.find(value => value.product === product).numberOfProduct === 0) {
+      this.cartProducts.splice(this.cartProducts.findIndex(value => value.product === product), 1);
     }
   }
 }
--- src/app/shared/services/cart.service.ts
+++ src/app/shared/services/cart.service.ts
@@ -9,18 +9,33 @@ export class CartService {
   productsInCart: Product[] = [];
 
   constructor() {
+    if (localStorage.getItem('cartProducts') != null) {
+      this.productsInCart = JSON.parse(this.getFromCart());
+    }
   }
 
   addToCart(product: Product) {
+    if (localStorage.getItem('cartProducts') != null) {
+      this.productsInCart = JSON.parse(this.getFromCart());
+    }
+
     this.productsInCart.push(product);
     localStorage.setItem('cartProducts', JSON.stringify(this.productsInCart));
   }
 
   getFromCart() {
-    return localStorage.getItem('cartProducts');
+    return localStorage['cartProducts'];
   }
 
-  removeFromCart() {
+  removeFromCart(product: Product) {
+    const cart_items = JSON.parse(localStorage['cartProducts']);
+    for (let i = 0; i < cart_items.length; i++) {
+      if (cart_items[i].id === product.id) {
+        cart_items.splice(i, 1);
 
+        break;
+      }
+    }
+    localStorage['cartProducts'] = JSON.stringify(cart_items);
   }
 }
