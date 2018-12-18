4f800c00da96c2743c6c2bb640e53b4f32229fda
Thomas <thom170b@easv365.dk>
2018-12-18 02:33:28 +0100

    Made product-add and product-update work with categories

--- src/app/admin/products/product-add/product-add.component.ts
+++ src/app/admin/products/product-add/product-add.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Router} from '@angular/router';
 import {ProductService} from '../../../shared/services/product.service';
+import {Product} from '../../../shared/models/product';
 
 @Component({
   selector: 'app-product-add',
@@ -10,16 +11,20 @@ import {ProductService} from '../../../shared/services/product.service';
 })
 export class ProductAddComponent implements OnInit {
 
+  products: Product[];
+  productCategories: string[] = [];
+
   productForm = new FormGroup({
     title: new FormControl(''),
     description: new FormControl(''),
     price: new FormControl(''),
     image: new FormControl(''),
     amountInStock: new FormControl(''),
+    existingCategory: new FormControl(''),
+    newCategory: new FormControl(''),
     featured: new FormControl('')
   });
   productFeatured: any;
-
 
   constructor(private productService: ProductService,
               private router: Router) {
@@ -27,15 +32,50 @@ export class ProductAddComponent implements OnInit {
 
   ngOnInit() {
     this.productFeatured = false;
+
+    this.productService.getProducts()
+      .subscribe(listOfProducts => {
+        this.products = listOfProducts;
+
+        this.productCategories = this.products.map(value => value.category)
+          .filter((value, index, array) => index === array.indexOf(value));
+      });
   }
 
   save() {
-    const product = this.productForm.value;
+    const product: Product = this.productForm.value;
+    if (this.productForm.get('newCategory').value === '') {
+      product.category = this.productForm.get('existingCategory').value;
+    } else {
+      product.category = this.productForm.get('newCategory').value;
+    }
     this.productService.addProduct(product)
       .subscribe(() => {
         this.router.navigateByUrl('/admin');
       });
   }
 
+  toggleExistingCategory() {
+    const newCategoryControl = this.productForm.get('newCategory');
 
+    const existingCategoryControl = this.productForm.get('existingCategory');
+    if (newCategoryControl.value === '') {
+      existingCategoryControl.enable();
+    } else {
+      existingCategoryControl.disable();
+      existingCategoryControl.setValue('');
+    }
+  }
+
+  toggleNewCategory() {
+    const existingCategoryControl = this.productForm.get('existingCategory');
+
+    const newCategoryControl = this.productForm.get('newCategory');
+    if (existingCategoryControl.value === 'none') {
+      newCategoryControl.enable();
+    } else {
+      newCategoryControl.disable();
+      newCategoryControl.setValue('');
+    }
+  }
 }
--- src/app/admin/products/product-update/product-update.component.ts
+++ src/app/admin/products/product-update/product-update.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {ProductService} from '../../../shared/services/product.service';
 import {FormControl, FormGroup} from '@angular/forms';
+import {Product} from '../../../shared/models/product';
 
 @Component({
   selector: 'app-product-update',
@@ -11,6 +12,8 @@ import {FormControl, FormGroup} from '@angular/forms';
 export class ProductUpdateComponent implements OnInit {
 
   productId: number;
+  products: Product[];
+  productCategories: string[] = [];
 
   productForm = new FormGroup({
     title: new FormControl(''),
@@ -18,6 +21,8 @@ export class ProductUpdateComponent implements OnInit {
     price: new FormControl(''),
     image: new FormControl(''),
     amountInStock: new FormControl(''),
+    existingCategory: new FormControl(''),
+    newCategory: new FormControl(''),
     featured: new FormControl('')
   });
   productFeatured: any;
@@ -30,27 +35,65 @@ export class ProductUpdateComponent implements OnInit {
   ngOnInit() {
     this.productId = +this.route.snapshot.paramMap.get('productId');
 
-    this.productService.getProductById(this.productId)
-      .subscribe(productFromRest => {
-        this.productForm.patchValue({
-          title: productFromRest.title,
-          description: productFromRest.description,
-          price: productFromRest.price,
-          image: productFromRest.image,
-          amountInStock: productFromRest.amountInStock,
-          featured: productFromRest.featured
-        });
-        this.productFeatured = productFromRest.featured;
+    this.productService.getProducts()
+      .subscribe(listOfProducts => {
+        this.products = listOfProducts;
+
+        this.productCategories = this.products.map(value => value.category)
+          .filter((value, index, array) => index === array.indexOf(value));
+
+        this.productService.getProductById(this.productId)
+          .subscribe(productFromRest => {
+            this.productForm.patchValue({
+              title: productFromRest.title,
+              description: productFromRest.description,
+              price: productFromRest.price,
+              image: productFromRest.image,
+              amountInStock: productFromRest.amountInStock,
+              featured: productFromRest.featured,
+              existingCategory: productFromRest.category
+            });
+            this.productFeatured = productFromRest.featured;
+          });
       });
   }
 
   save() {
-    const product = this.productForm.value;
+    const product: Product = this.productForm.value;
+    if (this.productForm.get('newCategory').value === '') {
+      product.category = this.productForm.get('existingCategory').value;
+    } else {
+      product.category = this.productForm.get('newCategory').value;
+    }
     product.productId = this.productId;
     this.productService.updateProduct(product)
       .subscribe(() => {
         this.router.navigateByUrl('/admin');
       });
+  }
+
+  toggleExistingCategory() {
+    const newCategoryControl = this.productForm.get('newCategory');
+
+    const existingCategoryControl = this.productForm.get('existingCategory');
+    if (newCategoryControl.value === '') {
+      existingCategoryControl.enable();
+    } else {
+      existingCategoryControl.disable();
+      existingCategoryControl.setValue('');
+    }
+  }
+
+  toggleNewCategory() {
+    const existingCategoryControl = this.productForm.get('existingCategory');
+
+    const newCategoryControl = this.productForm.get('newCategory');
+    if (existingCategoryControl.value === 'none') {
+      newCategoryControl.enable();
+    } else {
+      newCategoryControl.disable();
+      newCategoryControl.setValue('');
+    }
   }
 
 }
